7a55bcc0cc2dc8572d0eebed4603e51c2b9852c6
MIHIR PATEL <mihirptl89@gmail.com>
2015-08-23 10:32:39 -0600

    is authenticated; Auth Controller

--- server.js
+++ server.js
@@ -4,22 +4,26 @@
 var express = require('express');
 var mongoose = require('mongoose');
 var bodyParser = require('body-parser');
+var passport = require('passport');
 
 // Controllers
 var CoffeeCtrl = require("./controllers/CoffeeCtrl");
 var UserCtrl = require('./controllers/UserCtrl');
+var AuthCtrl = require('./controllers/AuthCtrl');
 
 // Initate our app
 var app = express();
 
-//Middleware
-app.use('/', bodyParser.json());
-
 // Express router
 var router = express.Router();
 
+//Middleware
+app.use('/', bodyParser.json());
 // Register all our routes with /api
 app.use('/api', router);
+// Use the passport package in our application
+app.use(passport.initialize());
+
 
 /********************************************
  - START ENDPOINTS -
@@ -27,19 +31,19 @@ app.use('/api', router);
 
 // Create endpoint handlers for /coffee
 router.route('/coffee')
-  .post(CoffeeCtrl.create)
-  .get(CoffeeCtrl.read);
+  .post(AuthCtrl.isAuthenticated, CoffeeCtrl.create)
+  .get(AuthCtrl.isAuthenticated, CoffeeCtrl.read);
 
 // Create endpoint handlers for /coffee/:coffee_id
 router.route('/coffee/:coffee_id')
-  .get(CoffeeCtrl.read)
-  .put(CoffeeCtrl.update)
-  .delete(CoffeeCtrl.remove);
+  .get(AuthCtrl.isAuthenticated, CoffeeCtrl.read)
+  .put(AuthCtrl.isAuthenticated, CoffeeCtrl.update)
+  .delete(AuthCtrl.isAuthenticated, CoffeeCtrl.remove);
 
 // Create endpoint handlers for /users
 router.route('/users')
   .post(UserCtrl.create)
-  .get(UserCtrl.read);
+  .get(AuthCtrl.isAuthenticated, UserCtrl.read);
 
 /********************************************
  - END ENDPOINTS -
